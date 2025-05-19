fe386163ea406dca96d01a4f44eb934cb80abbd1
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-05-19 14:31:17 +0200

    test: update tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -9,15 +9,15 @@ from mammos_spindynamics.db import get_spontaneous_magnetisation
 DATA_DIR = pathlib.Path(__file__).parent.resolve() / "data"
 
 
-def test_CrNiP():
-    """Test material `CrNiP`.
+def test_Co2Fe2H4():
+    """Test material `Co2Fe2H4`.
 
-    There is only one material with formula `CrNiP`, so this
+    There is only one material with formula `Co2Fe2H4`, so this
     test should load its table without issues.
     """
-    M = get_spontaneous_magnetisation(chemical_formula="CrNiP", print_info=False)
-    assert np.allclose(M(400), 186.92772987574517)
-    assert np.allclose(M(450), 0.5 * (186.92772987574517 + 205.8706929415473))
+    M = get_spontaneous_magnetisation(chemical_formula="Co2Fe2H4", print_info=False)
+    assert np.allclose(M(400), 990071.556207981)
+    assert np.allclose(M(450), 0.5 * (967365.0340483808 + 957943.7467350367))
 
 
 def test_NdFe14B():
@@ -30,8 +30,8 @@ def test_NdFe14B():
         get_spontaneous_magnetisation(chemical_formula="NdFe14B")
 
 
-def test_CrNiP_11():
-    """Test material `CrNiP` with space group number 11.
+def test_Co2Fe2H4_11():
+    """Test material `Co2Fe2H4` with space group number 11.
 
     There is no material with such formula and space group
     in the database, so we expect a `LookupError`.
