0a367057bae389e5ffe5acaf6d1664f4858160e9
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-11-04 14:09:51 +0100

    update - work in progress

--- src/mammos_spindynamics/uppasd.py
+++ src/mammos_spindynamics/uppasd.py
@@ -74,8 +74,8 @@ class Simulation:
     @property
     def volume(self) -> me.Entity:
         """Evaluate cell volume."""
-        with open(self.inpsd, encoding="utf-8") as f:
-            lines = f.readlines()
+        with open(self.inpsd, encoding="utf-8") as file:
+            lines = file.readlines()
             for i, line in enumerate(lines):
                 if fnmatch.fnmatch(line, "cell*"):
                     a = np.genfromtxt(StringIO(line[4:])) * u.Angstrom
@@ -90,18 +90,18 @@ class Simulation:
     @property
     def n_magnetic_atoms(self) -> int:
         """Evaluate number of magnetic atoms."""
-        with open(self.momfile, encoding="utf-8") as f:
-            n = len(f.readlines())
+        with open(self.momfile, encoding="utf-8") as file:
+            n = len(file.readlines())
         return n
 
     @property
     def get_T_from_inpsd(self) -> float:
         """Read temperature from inpsd.dat file."""
-        with open(self.inpsd, encoding="utf-8") as f:
-            lines = f.readlines()
+        with open(self.inpsd, encoding="utf-8") as file:
+            lines = file.readlines()
         for ll in lines:
             if fnmatch.fnmatch(ll, "temp *"):
-                T = ll.lstrip("temp ").strip()
+                T = ll.removeprefix("temp ").strip()
                 try:
                     T = float(T)
                 except ValueError as exc:
@@ -112,7 +112,7 @@ class Simulation:
     def run(
         self,
         T: float | u.Quantity | None = None,
-        outdir: pathlib.Path | str = "out",
+        out_dir: pathlib.Path | str = "out",
         **kwargs,
     ) -> None:
         """Run a UppASD calculation.
@@ -121,7 +121,7 @@ class Simulation:
         file inpsd.dat.
 
         In particular, a `run_0` directory will be created in the output directory
-        `outdir`. If a directory `run_0` already exists, the next available index will
+        `out_dir`. If a directory `run_0` already exists, the next available index will
         be used instead.
 
         The structure will look like this:
@@ -136,26 +136,26 @@ class Simulation:
         """
         if T is None:
             T = self.get_T_from_inpsd
-        outdir = pathlib.Path(outdir)
+        out_dir = pathlib.Path(out_dir)
         run_idx = (
             0
-            if not (outdir / "run_0").is_dir()
+            if not (out_dir / "run_0").is_dir()
             else max(
                 [
-                    int(i.name.lstrip("run_"))
-                    for i in outdir.iterdir()
+                    int(i.name.removeprefix("run_"))
+                    for i in out_dir.iterdir()
                     if fnmatch.fnmatch(i.name, "run_[0-9]")
                 ]
             )
             + 1
         )
-        run_dir = pathlib.Path(outdir) / f"run_{run_idx}"
+        run_dir = pathlib.Path(out_dir) / f"run_{run_idx}"
         run_dir.mkdir(exist_ok=True, parents=True)
         shutil.copy(self.jfile, run_dir)
         shutil.copy(self.momfile, run_dir)
         shutil.copy(self.posfile, run_dir)
-        with open(run_dir / "inpsd.dat", "w", encoding="utf-8") as f:
-            f.writelines(_parse_inpsd_lines(self.inpsd, temp=str(T), **kwargs))
+        with open(run_dir / "inpsd.dat", "w", encoding="utf-8") as file:
+            file.writelines(_parse_inpsd_lines(self.inpsd, temp=str(T), **kwargs))
         t_start = (
             datetime.datetime.now(datetime.UTC)
             .astimezone()
@@ -167,7 +167,7 @@ class Simulation:
             .astimezone()
             .isoformat(timespec="seconds")
         )
-        with open(run_dir / "info.json", "w") as f:
+        with open(run_dir / "info.json", "w") as file:
             json.dump(
                 {
                     "runner": "run",
@@ -176,19 +176,19 @@ class Simulation:
                     "time_end": t_end,
                     "mammos_spindynamics_version": mammos_spindynamics.__version__,
                 },
-                f,
+                file,
             )
 
     def run_temparray(
         self,
         T_array: list | np.ndarray,
-        outdir: pathlib.Path | str = "out",
+        out_dir: pathlib.Path | str = "out",
         **kwargs,
     ):
         """Run a series of UppASD calculations with a temperature array.
 
         In particular, a `run_temparray_0` directory will be created in the output
-        directory `outdir`. If a directory `run_temparray_0` already exists, the next
+        directory `out_dir`. If a directory `run_temparray_0` already exists, the next
         available index will be used instead.
 
         The structure will look like this:
@@ -205,20 +205,20 @@ class Simulation:
             |   |   +-- run_1/
 
         """
-        outdir = pathlib.Path(outdir)
+        out_dir = pathlib.Path(out_dir)
         run_idx = (
             0
-            if not (outdir / "run_temparray_0").is_dir()
+            if not (out_dir / "run_temparray_0").is_dir()
             else max(
                 [
-                    int(i.name.lstrip("run_temparray_"))
-                    for i in outdir.iterdir()
+                    int(i.name.removeprefix("run_temparray_"))
+                    for i in out_dir.iterdir()
                     if fnmatch.fnmatch(i.name, "run_temparray_[0-9]")
                 ]
             )
             + 1
         )
-        run_temparray_dir = pathlib.Path(outdir) / f"run_temparray_{run_idx}"
+        run_temparray_dir = pathlib.Path(out_dir) / f"run_temparray_{run_idx}"
         run_temparray_dir.mkdir(exist_ok=True, parents=True)
         t_start = (
             datetime.datetime.now(datetime.UTC)
@@ -228,7 +228,7 @@ class Simulation:
         for T in T_array:
             self.run(
                 T=T,
-                outdir=run_temparray_dir,
+                out_dir=run_temparray_dir,
                 **kwargs,
             )
         t_end = (
@@ -236,7 +236,7 @@ class Simulation:
             .astimezone()
             .isoformat(timespec="seconds")
         )
-        with open(run_temparray_dir / "info.json", "w") as f:
+        with open(run_temparray_dir / "info.json", "w") as file:
             json.dump(
                 {
                     "runner": "run_temparray",
@@ -245,73 +245,30 @@ class Simulation:
                     "time_end": t_end,
                     "mammos_spindynamics_version": mammos_spindynamics.__version__,
                 },
-                f,
+                file,
             )
 
 
-def read_result(outdir: pathlib.Path | str) -> ResultCollection:
+# def read_result(...):  # ?
+# def read_out_dir(...):  # ?
+def get_ResultCollection(out_dir: pathlib.Path | str) -> ResultCollection:
     """Read UppASD calculations results directory."""
-    return ResultCollection(outdir)
-    # outdir = pathlib.Path(outdir)
-    # kB = u.constants.k_B.to("mRy/K").value  # Boltzmann constant in [mRy/K]
-    # data = []
-    # for run_dir in outdir.iterdir():
-    #     if "run_" in run_dir.name:
-    #         temp = float(run_dir.name.lstrip("run_"))
-    #         cumulant_files = [
-    #             f
-    #             for f in run_dir.iterdir()
-    #             if fnmatch.fnmatch(f.name, "cumulant*.out")
-    #         ]
-    #         if cumulant_files:
-    #             cumulant_files.sort()
-    #             df = pd.read_csv(cumulant_files[-1], sep=r"\s+")
-    #             row = pd.concat([pd.Series({"T": temp}), df.iloc[-1]])
-    #             data.append(row)
-    # df = pd.DataFrame(data)
-    # df["C_v[K_B]"] = np.gradient(df["<E>"] / kB, df["T"], axis=0)
-    # df.to_csv(outdir / "M(T)_df", index=False, float_format="%.8E")
-    # out = df[["T", "<M>", "U_{Binder}", "C_v[K_B]"]].rename(
-    #     columns={"<M>": "<M>[μB]"}
-    # )
-    # np.savetxt(
-    #     outdir / "M(T)",
-    #     out.to_numpy(),
-    #     fmt=["%04d"] + ["% .8E"] * 3,
-    #     header="T[K] " + "".join([f"{h:^16}" for h in out.columns[1:]]),
-    #     comments="",
-    # )
-    # me.io.entities_to_file(
-    #     outdir / "output.csv",
-    #     "Magnetization and heat capacity from UppASD",
-    #     T=me.Entity("ThermodynamicTemperature", out["T"].to_numpy()),
-    #     Ms=me.Ms(
-    #         out["<M>[μB]"].to_numpy()
-    #         * self.n_magnetic_atoms
-    #         * u.constants.muB
-    #         / self.volume.q,
-    #         unit="A/m",
-    #     ),
-    #     U_binder=out["U_{Binder}"].to_numpy(),
-    #     Cv=me.Entity(
-    #         "IsochoricHeatCapacity", out["C_v[K_B]"].to_numpy() * u.constants.k_B
-    #     ),
-    # )
+    return ResultCollection(out_dir)
 
 
 class ResultCollection:
     """Collection of UppASD Result instances."""
 
-    def __init__(self, outdir: pathlib.Path):
-        """Initialize ResultCollection instance."""
-        self.outdir = pathlib.Path(outdir)
+    def __init__(self, out_dir: pathlib.Path):
+        """Initialize ResultCollection given the output directory."""
+        self.out_dir = pathlib.Path(out_dir)
         runs = []
-        for d_ in self.outdir.iterdir():
+        for d_ in self.out_dir.iterdir():
             if fnmatch.fnmatch(d_.name, "run_[0-9]*") or fnmatch.fnmatch(
                 d_.name, "run_temparray_[0-9]*"
             ):
-                with open(d_ / "info.json") as f:
-                    info = json.load(f)
+                with open(d_ / "info.json") as file:
+                    info = json.load(file)
                 runs.append({"id": d_.name, **info})
         self.runs = runs
 
@@ -322,9 +279,15 @@ class ResultCollection:
 
     def __getitem__(self, idx):
         """Extract i-th run."""
-        return Result(self.outdir / self.list_runs[idx])
+        if self.runs[idx]["runner"] == "run":
+            return Result(self.out_dir / self.runs[idx]["id"])
+        elif self.runs[idx]["runner"] == "run_temparray":
+            return TempArrayResult(self.out_dir / self.runs[idx]["id"])
+        else:
+            raise RuntimeError("Runner not recognized.")
 
-    def __repr__(self):
+    def __str__(self):
+        """Dunder method for the `print()` function."""
         return repr(self.dataframe)
 
     def _repr_html_(self):
@@ -334,74 +297,145 @@ class ResultCollection:
 class Result:
     """UppASD Result parser class."""
 
-    rundir: pathlib.Path
+    def __init__(self, run_dir: pathlib.Path):
+        """Initialize Result given the run directory."""
+        self.run_dir = pathlib.Path(run_dir)
+        with open(self.run_dir / "info.json") as file:
+            info = json.load(file)
+        self.T = info["temperature"]
+        df = pd.read_csv(self.last_cumulant, sep=r"\s+")
+        self.data = df.iloc[-1]
 
     @property
     def inpsd(self) -> str:
-        """Return inpsd.dat file."""
-        with open(self.rundir / "inpsd.dat") as f:
-            out = f.read()
-        return out
+        """Return path of ``inpsd.dat`` file."""
+        return self.run_dir / "inpsd.dat"
 
-    def save_output(self, path: pathlib.Path | str | None = None) -> None:
+    @property
+    def jfile(self) -> str:
+        """Return path of ``jfile`` file."""
+        return self.run_dir / "jfile"
+
+    @property
+    def momfile(self) -> str:
+        """Return path of ``momfile`` file."""
+        return self.run_dir / "momfile"
+
+    @property
+    def posfile(self) -> str:
+        """Return path of ``posfile`` file."""
+        return self.run_dir / "posfile"
+
+    @property
+    def last_cumulant(self) -> str:
+        """Return last ``cumulant*.out`` file."""
+        cumulant_files = [
+            f
+            for f in self.run_dir.iterdir()
+            if fnmatch.fnmatch(f.name, "cumulant*.out")
+        ]
+        if not cumulant_files:
+            raise ValueError(
+                f"No cumulant files found in the `run_dir` {self.run_dir}."
+            )
+        else:
+            cumulant_files.sort()
+            return cumulant_files[-1]
+
+    @property
+    def volume(self) -> me.Entity:
+        """Evaluate cell volume."""
+        with open(self.inpsd, encoding="utf-8") as file:
+            lines = file.readlines()
+            for i, line in enumerate(lines):
+                if fnmatch.fnmatch(line, "cell*"):
+                    a = np.genfromtxt(StringIO(line[4:])) * u.Angstrom
+                    b = np.genfromtxt(StringIO(lines[i + 1])) * u.Angstrom
+                    c = np.genfromtxt(StringIO(lines[i + 2])) * u.Angstrom
+                    V = me.Entity("CellVolume", np.dot(a, np.cross(b, c)))
+                    break
+            else:
+                raise RuntimeError("Cell volume could not be evaluated.")
+        return V
+
+    @property
+    def n_magnetic_atoms(self) -> int:
+        """Evaluate number of magnetic atoms."""
+        with open(self.momfile, encoding="utf-8") as file:
+            n = len(file.readlines())
+        return n
+
+
+class TempArrayResult:
+    """Class for the result of the temparray runner."""
+
+    def __init__(self, run_dir: pathlib.Path | str):
+        """Initialize TempArrayResult given the run directory."""
+        self.run_dir = pathlib.Path(run_dir)
+        with open(self.run_dir / "info.json") as file:
+            info = json.load(file)
+        self.T_array = info["temperature"]
+        self.sub_runs = []
+        for sub_run_dir in self.run_dir.iterdir():
+            if "run_" in sub_run_dir.name:
+                self.sub_runs.append(Result(sub_run_dir))
+
+    @property
+    def volume(self) -> me.Entity:
+        """Evaluate cell volume."""
+        return self.sub_runs[0].volume
+
+    @property
+    def n_magnetic_atoms(self) -> int:
+        """Evaluate number of magnetic atoms."""
+        return self.sub_runs[0].n_magnetic_atoms
+
+    @property
+    def dataframe(self) -> pandas.DataFrame:
+        """Dataframe containing information of the temparray run."""
+        kB = u.constants.k_B.to("mRy/K").value  # Boltzmann constant in [mRy/K]
+        list_data = []
+        for sub_run in self.sub_runs:
+            list_data.append(pd.concat([pd.Series({"T": sub_run.T}), sub_run.data]))
+        df = pd.DataFrame(list_data)
+        df["C_v[K_B]"] = np.gradient(df["<E>"] / kB, df["T"], axis=0)
+        df = df.rename(columns={"<M>": "<M>[μB]", "#Iter": "iter"})
+        return df
+
+    def save_output(self, out_dir: pathlib.Path | str | None = None) -> None:
         """Save output files M(T) and output.csv.
 
         The generated files are `M(T)` and `output.csv`.
-        The first has the following structure:
-
-        .. code-block::
-
-            T[K]     <M>[μB]        U_{Binder}       C_v[K_B]
-            0001  0.12345678E-90  1.23456789E-01  2.34567890E+12
-            0002  0.12345678E-90  1.23456789E-01  2.34567890E+12
-
-        The file `output.csv` is generated from :py:mod:`mammos_entity.io` using
-        information from `M(T)` and converting to ontology units.
-
+        The first one contains all information evaluated from the cumulant files.
+        The file `output.csv` is instead generated from :py:mod:`mammos_entity.io` using
+        information from `M(T)` and converting all quantities to ontology units.
         """
-        path = pathlib.Path(path)
-        kB = u.constants.k_B.to("mRy/K").value  # Boltzmann constant in [mRy/K]
-        data = []
-        for run_dir in outdir.iterdir():
-            if "run_" in run_dir.name:
-                temp = float(run_dir.name.lstrip("run_"))
-                cumulant_files = [
-                    f
-                    for f in run_dir.iterdir()
-                    if fnmatch.fnmatch(f.name, "cumulant*.out")
-                ]
-                if cumulant_files:
-                    cumulant_files.sort()
-                    df = pd.read_csv(cumulant_files[-1], sep=r"\s+")
-                    row = pd.concat([pd.Series({"T": temp}), df.iloc[-1]])
-                    data.append(row)
-        df = pd.DataFrame(data)
-        df["C_v[K_B]"] = np.gradient(df["<E>"] / kB, df["T"], axis=0)
-        df.to_csv(outdir / "M(T)_df", index=False, float_format="%.8E")
-        out = df[["T", "<M>", "U_{Binder}", "C_v[K_B]"]].rename(
-            columns={"<M>": "<M>[μB]"}
-        )
+        if out_dir is None:
+            out_dir = self.run_dir
+        out_dir = pathlib.Path(out_dir)
         np.savetxt(
-            outdir / "M(T)",
-            out.to_numpy(),
-            fmt=["%04d"] + ["% .8E"] * 3,
-            header="T[K] " + "".join([f"{h:^16}" for h in out.columns[1:]]),
+            out_dir / "M(T)",
+            self.dataframe.to_numpy(),
+            fmt=["%04d"] * 2 + ["% .8E"] * (self.dataframe.shape[1] - 2),
+            header="T[K] iter"
+            + "".join([f"{h:^16}" for h in self.dataframe.columns[2:]]),
             comments="",
         )
         me.io.entities_to_file(
-            outdir / "output.csv",
+            out_dir / "output.csv",
             "Magnetization and heat capacity from UppASD",
-            T=me.Entity("ThermodynamicTemperature", out["T"].to_numpy()),
+            T=me.Entity("ThermodynamicTemperature", self.dataframe["T"].to_numpy()),
             Ms=me.Ms(
-                out["<M>[μB]"].to_numpy()
+                self.dataframe["<M>[μB]"].to_numpy()
                 * self.n_magnetic_atoms
                 * u.constants.muB
                 / self.volume.q,
                 unit="A/m",
             ),
-            U_binder=out["U_{Binder}"].to_numpy(),
+            U_binder=self.dataframe["U_{Binder}"].to_numpy(),
             Cv=me.Entity(
-                "IsochoricHeatCapacity", out["C_v[K_B]"].to_numpy() * u.constants.k_B
+                "IsochoricHeatCapacity",
+                self.dataframe["C_v[K_B]"].to_numpy() * u.constants.k_B,
             ),
         )
 
@@ -412,8 +446,8 @@ def _parse_inpsd_lines(inpsd: pathlib.Path | str, **kwargs):
     If a couple `key: value` is given as keyword argument, the line beginning with
     `key` in the `inpsd.dat` will be assigned the value `value`.
     """
-    with open(inpsd, encoding="utf-8") as f:
-        lines = f.readlines()
+    with open(inpsd, encoding="utf-8") as file:
+        lines = file.readlines()
     new_lines = []
     for ll in lines:
         for key, val in kwargs.items():
